a7363d1dc7d1c6960fa232fae3fa865858d8b0c6
omohemma <emmaomoh@gmail.com>
2020-03-17 04:53:15 +0100

    Optimizing your Editor

---
+++ public/src/components/EventCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- The Keyword vue to scaffold  -->
+  <h1></h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I want to be free'
+    }
+  }
+}
+</script>
+
+<style></style>
